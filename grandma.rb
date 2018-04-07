eb7923222fd3d0190a3f82a49488ead5594fe3dd
Aaron Na <syn228@nyu.edu>
2018-04-07 21:43:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(string)
-  if (!string.upcase)
+  if (string != string.upcase)
     return "HUH?! SPEAK UP, SONNY!"
-  elseif (string.upcase)
+  elseif (string == string.upcase)
     return "NO, NOT SINCE 1938!"
   elseif (string == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
