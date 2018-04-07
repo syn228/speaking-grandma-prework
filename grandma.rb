f1cf3540323566c42089da6275cad4009cc8f11e
Aaron Na <syn228@nyu.edu>
2018-04-07 21:51:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,10 +3,10 @@
 def speak_to_grandma(string)
   if (string == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
-  elseif (string == "Hi!")
-    return "HUH?! SPEAK UP, SONNY!"
   elseif (string == string.upcase)
     return "NO, NOT SINCE 1938!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!"
 
   end
 end
